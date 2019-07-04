d81902b7f6de43c661edfb0d8e3ecd824f97df95
Esteban Solorzano <david.solorzano@atlanticsoft.co>
2019-07-04 10:48:18 -0500

    Updates

---
+++ precache-manifest.7b94bc1a0c80f37b456fd797949c7f52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd32fbb71e1f381d226a1ed1b6f2913d",
+    "url": "/ReactVideoPlayer/index.html"
+  },
+  {
+    "revision": "f42ad1e73e278de8dd8b",
+    "url": "/ReactVideoPlayer/static/css/main.f4282185.chunk.css"
+  },
+  {
+    "revision": "df06d76b86ad3a696004",
+    "url": "/ReactVideoPlayer/static/js/2.3e87af67.chunk.js"
+  },
+  {
+    "revision": "f42ad1e73e278de8dd8b",
+    "url": "/ReactVideoPlayer/static/js/main.8db58047.chunk.js"
+  },
+  {
+    "revision": "1090c8a43a8f82777d87",
+    "url": "/ReactVideoPlayer/static/js/runtime~main.0363b1cc.js"
+  }
+]);
